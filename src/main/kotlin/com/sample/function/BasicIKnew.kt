13db6c63619072a456cb25d656e6d5f0dc7f2d15
Nattaporn Chatmalairut <nattaporn.cha@ascendcorp.com>
2019-05-26 14:40:42 +0700

    Second function is sample receive function as parameter, that function received int value and return string.

--- src/main/kotlin/com/sample/function/BasicIKnew.kt
+++ src/main/kotlin/com/sample/function/BasicIKnew.kt
@@ -5,9 +5,22 @@ class BasicIKnew {
         val fn: (Int) -> String = { i -> i.toString() }
         return fn.invoke(n)
     }
+
+    // received function as parameter
+    // (Int) -> String :: Function received integer value and return to string
+    fun convIntToMessage(i: Int, fn: (Int) -> String): String {
+        return fn.invoke(i)
+    }
 }
 
 fun main(args: Array<String>) {
     val demo = BasicIKnew()
+
     println(demo.convToString(1))
+
+    val intToMessage = demo.convIntToMessage(
+        1,
+        fn = { i -> "function received function as parameter and convert to string message $i" })
+
+    println(intToMessage)
 }
