f7ded178fa8e56fa663726c465d054683f6f1d42
Nattaporn Chatmalairut <nattaporn.cha@ascendcorp.com>
2019-05-29 15:35:54 +0700

    Let's start with useful data type `Either`.

---
+++ src/test/kotlin/com/sample/function/arrow/LearnUseEither.kt
@@ -0,0 +1,30 @@
+package com.sample.function.arrow
+
+import arrow.core.Either
+import io.kotlintest.shouldBe
+import io.kotlintest.shouldNotBe
+import io.kotlintest.specs.DescribeSpec
+
+class LearnUseEither : DescribeSpec({
+    describe("""Either, learn to use arrow-kt data type "Either" """) {
+        // Let's start with how to initiate an instance of either data type.
+        // Right side for successful value
+        val r = Either.right("success")
+        // Left side for failure value
+        val l = Either.left("failure")
+        it("should return true if right is right side") {
+            r.isRight() shouldBe true
+        }
+        it("should return false if right is not left side") {
+            r.isLeft() shouldNotBe true
+        }
+        it("should return true if left is left side") {
+            l.isLeft() shouldBe true
+        }
+
+        it("should return false if left is not right side") {
+            l.isRight() shouldNotBe true
+        }
+
+    }
+})
