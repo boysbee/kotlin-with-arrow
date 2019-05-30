02d061051588644b27d2ffd05d98539b322015d0
Nattaporn Chatmalairut <nattaporn.cha@ascendcorp.com>
2019-05-30 17:16:21 +0700

    Add more stubbing code for failure and success.

--- src/main/kotlin/com/sample/function/arrow/LearnTry.kt
+++ src/main/kotlin/com/sample/function/arrow/LearnTry.kt
@@ -1,15 +1,30 @@
 package com.sample.function.arrow
 
+import java.util.concurrent.ThreadLocalRandom
+
 
 open class CustomException : Exception()
 
 class BadRequestException : CustomException()
 
-fun requestOtp() {
-    validateRequest()
-
+fun requestOtpFailure(): String? {
+    validateRequestFailure()
+    return null
 }
 
-fun validateRequest() {
+fun requestOtpSuccessfully(): String {
+    validateRequest()
+    return generateOTP()
+}
+
+fun validateRequestFailure() {
     throw BadRequestException()
+}
+
+fun validateRequest(): Boolean {
+    return true
+}
+
+fun generateOTP(): String {
+    return ThreadLocalRandom.current().nextInt(0, 9999).toString()
 }
