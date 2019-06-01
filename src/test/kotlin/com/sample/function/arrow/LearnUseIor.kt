2cacd12e358c42466d601f26205dcfb349818c5b
Nattaporn Chatmalairut <nattaporn.cha@ascendcorp.com>
2019-06-01 23:10:02 +0700

    Add case Ior with left side.

--- src/test/kotlin/com/sample/function/arrow/LearnUseIor.kt
+++ src/test/kotlin/com/sample/function/arrow/LearnUseIor.kt
@@ -5,6 +5,7 @@ import arrow.data.Ior
 import arrow.data.getOrElse
 import io.kotlintest.assertSoftly
 import io.kotlintest.shouldBe
+import io.kotlintest.shouldNotBe
 import io.kotlintest.specs.DescribeSpec
 
 class LearnUseIor : DescribeSpec({
@@ -14,7 +15,20 @@ class LearnUseIor : DescribeSpec({
             val r = Ior.Right("Ok")
             assertSoftly {
                 r.isRight shouldBe true
+                r.isLeft shouldNotBe true
+                r.isBoth shouldNotBe true
                 r.map { "It is $it" }.getOrElse { None } shouldBe "It is Ok"
+            }
+        }
+
+        it("""should be return "Left" side """) {
+            val l = Ior.Left("Failure")
+            assertSoftly {
+                l.isLeft shouldBe true
+                l.isRight shouldNotBe true
+                l.isBoth shouldNotBe true
+                // Left side can not map a value in container
+                l.map { "It is $it" }.getOrElse { None } shouldBe None
             }
         }
     }
