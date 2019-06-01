440ce70524ab9abf558401b5465e6723b58da70f
Nattaporn Chatmalairut <nattaporn.cha@ascendcorp.com>
2019-06-02 00:46:56 +0700

    Add case Ior initiate with extension.

--- src/test/kotlin/com/sample/function/arrow/LearnUseIor.kt
+++ src/test/kotlin/com/sample/function/arrow/LearnUseIor.kt
@@ -1,8 +1,7 @@
 package com.sample.function.arrow
 
 import arrow.core.None
-import arrow.data.Ior
-import arrow.data.getOrElse
+import arrow.data.*
 import io.kotlintest.assertSoftly
 import io.kotlintest.shouldBe
 import io.kotlintest.shouldNotBe
@@ -40,6 +39,31 @@ class LearnUseIor : DescribeSpec({
                 b.isBoth shouldBe true
                 // Both side can not map a value in Right side like Either.
                 b.map { "It is $it" }.getOrElse { None } shouldBe "It is Ok"
+            }
+        }
+    }
+    describe("""Initiate "Ior" from extension""") {
+        it("""should initiate from method "rightIor" to "Ior.Right" """) {
+            val r = "Success".rightIor()
+            assertSoftly {
+                r.isRight shouldBe true
+                r shouldBe Ior.Right("Success")
+            }
+        }
+
+        it("""should initiate from method "leftIor" to "Ior.Left" """) {
+            val l = "Failure".leftIor()
+            assertSoftly {
+                l.isLeft shouldBe true
+                l shouldBe Ior.Left("Failure")
+            }
+        }
+
+        it("""should initiate from method "bothIor" to "Ior.Both" """) {
+            val b = ("Failure" to "Success").bothIor()
+            assertSoftly {
+                b.isBoth shouldBe true
+                b shouldBe Ior.Both("Failure", "Success")
             }
         }
     }
