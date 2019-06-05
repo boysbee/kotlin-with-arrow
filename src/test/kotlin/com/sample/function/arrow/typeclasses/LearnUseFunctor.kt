2796820e37a1076273cf70f744197f866bc22e16
Nattaporn_cha <nattaporn.cha@ascendcorp.com>
2019-06-05 21:05:24 +0700

    Learn Functor from unit test with first test Option.

---
+++ src/test/kotlin/com/sample/function/arrow/typeclasses/LearnUseFunctor.kt
@@ -0,0 +1,24 @@
+package com.sample.function.arrow.typeclasses
+
+import arrow.core.Option
+import arrow.core.Some
+import arrow.core.Try
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.FreeSpec
+
+class LearnUseFunctor : FreeSpec({
+    /**
+     * The Functor typeclass abstracts the ability to map over the computational context of a type constructor
+     *
+     * fun F<A>.map(f: (A) -> B): F<B>
+     */
+    """Learn learn Functor""" - {
+        """Option.map""" {
+            // F is Option
+            // A is String
+            // B is Int
+            val result: Option<Int> = Option<String>("2").map { it.toInt() }
+            result shouldBe Some(2)
+        }
+    }
+})
