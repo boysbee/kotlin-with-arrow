ad5d9ab18fd4995a85e5012ea2263f2ca7b7eaf8
Nattaporn_cha <nattaporn.cha@ascendcorp.com>
2019-06-05 21:14:46 +0700

    Add unit test Either functor.

--- src/test/kotlin/com/sample/function/arrow/typeclasses/LearnUseFunctor.kt
+++ src/test/kotlin/com/sample/function/arrow/typeclasses/LearnUseFunctor.kt
@@ -1,8 +1,7 @@
 package com.sample.function.arrow.typeclasses
 
-import arrow.core.Option
-import arrow.core.Some
-import arrow.core.Try
+import arrow.core.*
+import arrow.core.extensions.option.semiring.empty
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.FreeSpec
 
@@ -28,6 +27,15 @@ class LearnUseFunctor : FreeSpec({
             // Try<String>.map(f: (String) -> Int): Try<Int>
             val result: Try<Int> = Try<String> { "2" }.map { it.toInt() }
             result shouldBe Try.Success(2)
+        }
+        """Either.map""" {
+            // F is Either
+            // A is String
+            // B is String
+            // C is Int
+            // Either<String,String>.map(f: (String) -> Int): Either<String,Int>
+            val result: Either<String, Int> = Either.cond(true, { "2" }, { "0" }).map { it.toInt() }
+            result shouldBe Either.Right(2)
         }
     }
 })
