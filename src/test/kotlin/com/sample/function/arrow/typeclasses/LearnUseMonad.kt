0382854d8dc6b2d9c3a9ff2b2c38736f753654f4
Nattaporn Chatmalairut <nattaporn.cha@ascendcorp.com>
2019-06-07 14:29:47 +0700

    Learn use Monad typeclasses, let start with Option first with method "flatMap".

---
+++ src/test/kotlin/com/sample/function/arrow/typeclasses/LearnUseMonad.kt
@@ -0,0 +1,21 @@
+package com.sample.function.arrow.typeclasses
+
+import arrow.core.Option
+import arrow.core.Some
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.DescribeSpec
+
+class LearnUseMonad : DescribeSpec({
+    /**
+     * Monad is a typeclass that abstracts over sequential execution of code.
+     */
+    describe("Option flatmap") {
+        it("""should result is Some(3) when computation value of Some(a) and Some(b)""") {
+            val a: Option<Int> = Option.just(1)
+            val b: Option<Int> = Option.just(2)
+            val result = a.flatMap { f1 -> b.flatMap { f2 -> Some(f1 + f2) } }
+            result shouldBe Some(3)
+        }
+
+    }
+})
