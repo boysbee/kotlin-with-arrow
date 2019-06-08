f8de0268e5b5840d26285d6314154404614092ea
Nattaporn_cha <nattaporn.cha@ascendcorp.com>
2019-06-08 23:10:39 +0700

    monad with Try.flatMap Success will return Success.

--- src/test/kotlin/com/sample/function/arrow/typeclasses/LearnUseMonad.kt
+++ src/test/kotlin/com/sample/function/arrow/typeclasses/LearnUseMonad.kt
@@ -3,6 +3,7 @@ package com.sample.function.arrow.typeclasses
 import arrow.core.None
 import arrow.core.Option
 import arrow.core.Some
+import arrow.core.Try
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.DescribeSpec
 
@@ -10,7 +11,7 @@ class LearnUseMonad : DescribeSpec({
     /**
      * Monad is a typeclass that abstracts over sequential execution of code.
      */
-    describe("Option flatmap") {
+    describe("Option flatMap") {
         it("""should result is Some(3) when computation value of Some(a) and Some(b)""") {
             val a: Option<Int> = Option.just(1)
             val b: Option<Int> = Option.just(2)
@@ -18,7 +19,7 @@ class LearnUseMonad : DescribeSpec({
             result shouldBe Some(3)
         }
 
-        it("""should be None when flatmap None""") {
+        it("""should be None when flatMap None""") {
             None.flatMap { f1 -> Some("Not none") } shouldBe None
         }
 
@@ -29,4 +30,15 @@ class LearnUseMonad : DescribeSpec({
             result shouldBe None
         }
     }
+
+    describe("Try flatMap") {
+        it("""should result is Try.Success(3) when computation value of Try.Success(1) and Try.Success(2)""") {
+            val a: Try<Int> = Try { 1 }
+            val b: Try<Int> = Try { 2 }
+            val result = a.flatMap { f1 -> b.flatMap { f2 -> Try.Success( f1 + f2) } }
+            result shouldBe Try.Success(3)
+        }
+    }
+
+
 })
