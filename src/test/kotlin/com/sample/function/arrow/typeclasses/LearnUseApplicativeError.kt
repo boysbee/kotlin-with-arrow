d42ac3784ebbba73229ca0cae330dc0e6e5ab4ef
Nattaporn_cha <nattaporn.cha@ascendcorp.com>
2019-06-13 23:01:43 +0700

    Learn use "ApplicativeError" by "Try" datatype first.

---
+++ src/test/kotlin/com/sample/function/arrow/typeclasses/LearnUseApplicativeError.kt
@@ -0,0 +1,32 @@
+package com.sample.function.arrow.typeclasses
+
+import arrow.Kind
+import arrow.core.*
+import arrow.core.extensions.`try`.applicativeError.applicativeError
+import arrow.core.extensions.either.applicative.ap
+import arrow.core.extensions.either.applicative.just
+import arrow.core.extensions.either.foldable.get
+import arrow.core.extensions.either.monad.flatten
+import arrow.core.extensions.option.applicative.applicative
+import arrow.core.extensions.option.applicative.map2
+import arrow.data.ListK
+import arrow.data.k
+import com.sample.function.arrow.datatypes.BadRequestException
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.DescribeSpec
+import kotlin.Function1
+
+class LearnUseApplicativeError : DescribeSpec({
+
+    /**
+     * A.raiseError, A constructor function. It lifts an exception into the computational context of a type constructor.
+     *
+     */
+
+    describe("Try.raiseError") {
+        it("""should be return Try.Failure when Try<A>.raiseError(throwable)""") {
+            Try.applicativeError().raiseError<String>(BadRequestException()).isFailure() shouldBe true
+        }
+    }
+
+})
