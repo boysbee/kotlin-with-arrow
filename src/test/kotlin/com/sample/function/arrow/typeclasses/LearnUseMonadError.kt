4ae4daa3db491dc7bc4cb6e0287fa528b219aabd
Nattaporn_cha <nattaporn.cha@ascendcorp.com>
2019-06-17 23:38:12 +0700

    Learn "raiseError" with Either in MonadError.

--- src/test/kotlin/com/sample/function/arrow/typeclasses/LearnUseMonadError.kt
+++ src/test/kotlin/com/sample/function/arrow/typeclasses/LearnUseMonadError.kt
@@ -1,7 +1,9 @@
 package com.sample.function.arrow.typeclasses
 
+import arrow.core.Either
 import arrow.core.Try
 import arrow.core.extensions.`try`.applicativeError.raiseError
+import arrow.core.extensions.either.applicativeError.raiseError
 import com.sample.function.arrow.datatypes.BadRequestException
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.DescribeSpec
@@ -14,9 +16,17 @@ class LearnUseMonadError : DescribeSpec({
      */
 
     describe("Try.raiseError") {
-        it("""should be true return Error.raiseError""") {
+        it("""should be true return Error.raiseError is failure""") {
             val tryRequest: Try<String> = BadRequestException().raiseError()
             tryRequest.isFailure() shouldBe true
+        }
+    }
+
+    describe("Either.raiseError") {
+        it("""should be true return Error.raiseError is left""") {
+//            val eitherRequest: Either<BadRequestException, String> = BadRequestException().raiseError<Either<BadRequestException,String>>() as Either<BadRequestException, String>
+            var either:Either<BadRequestException,String> = BadRequestException().raiseError<BadRequestException, String>()
+            either.isLeft() shouldBe true
         }
     }
 
